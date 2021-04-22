d11a36e6d3881a0fe4630c949bf4d328a48552d3
Jenner Dulce <jennerdulce@gmail.com>
2021-04-21 20:15:56 -0700

    lambda post-jdd is fully functional

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 'use strict';
 
 const clientsModel = require('./schema.js');
+const uuid = require('uuid').v4;
 
 exports.handler = async (event) => {
+  console.log('EVENT', event)
   try {
     let body = JSON.parse(event.body);
     const id = uuid();
-    const {name, phone} = body;
-    console.log('EVENT', event)
-    console.log('BODY', body)
-    const record = new clientsModel({id, name, phone})
+    const { name, phone } = body;
+    
+    const record = new clientsModel({ id, name, phone })
+    const data = await record.save();
+    const savedRecord = JSON.stringify(data)
     return {
-      statusCode: 200,
-      body: data
+      statusCode: 201,
+      body: savedRecord
     }
   } catch (e) {
     return {
-      statusCode: 200,
+      statusCode: 500,
       body: e.message
     }
   }
-
 }
